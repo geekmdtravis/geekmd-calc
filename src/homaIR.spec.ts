23700c01b78af962dbb7563c09c277d66d48ec95
geekmdtravis <travis@geekmd.io>
2023-08-18 15:54:59 -0700

    homa ir code coveragfe to 100% of lines

--- src/homaIR.spec.ts
+++ src/homaIR.spec.ts
@@ -18,14 +18,36 @@ describe('homaIR', () => {
       homaIR({ glucose: 0, insulin: 104, fasting: true })
     ).toThrowError();
   });
-  it('calculates the score appropriately within a reasonable variance', () => {
+  it('reports insulin sensitivity when the score is less than 1', () => {
     const { value, interpretation } = homaIR({
-      glucose: 12.3,
-      insulin: 104,
+      glucose: 85,
+      insulin: 3,
       fasting: true,
     });
-    const expected = 3.2;
+    const expected = 0.6;
 
+    expect(value).toBeLessThan(expected + variance);
+    expect(value).toBeGreaterThan(expected - variance);
+    expect(interpretation).toBe('insulin sensitive');
+  });
+  it('reports insulin resistance when the score is between 1 and 2', () => {
+    const { value, interpretation } = homaIR({
+      glucose: 85,
+      insulin: 8,
+      fasting: true,
+    });
+    const expected = 1.7;
+    expect(value).toBeLessThan(expected + variance);
+    expect(value).toBeGreaterThan(expected - variance);
+    expect(interpretation).toBe('insulin resistance');
+  });
+  it('reports significant insulin resistance when the score is greater than 2', () => {
+    const { value, interpretation } = homaIR({
+      glucose: 85,
+      insulin: 15,
+      fasting: true,
+    });
+    const expected = 3.1;
     expect(value).toBeLessThan(expected + variance);
     expect(value).toBeGreaterThan(expected - variance);
     expect(interpretation).toBe('significant insulin resistance');
