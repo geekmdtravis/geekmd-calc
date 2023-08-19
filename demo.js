b533cc3c5914860cc55b06aa93644b307fde9f29
geekmdtravis <travis@geekmd.io>
2023-08-18 17:21:29 -0700

    updated demo

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
 const { homaIR } = require('./dist/');
 
+// Produces both a value and an interpretation for the user
 const { value, interpretation } = homaIR({
   glucose: 100,
   insulin: 10,
@@ -7,3 +8,14 @@ const { value, interpretation } = homaIR({
 });
 
 console.log(`HOMA-IR: ${value} (${interpretation})`);
+
+// Throws errors for invalid inputs
+try {
+  homaIR({
+    glucose: 0,
+    insulin: 0,
+    fasting: false,
+  });
+} catch (e) {
+  console.error(`Error: ${e.message}`);
+}
