2067f1b972e09fe90871d78e145d34a8a912e4ce
geekmdtravis <travis@geekmd.io>
2023-08-18 17:25:40 -0700

    updated demo

--- demo.js
+++ demo.js
@@ -9,13 +9,31 @@ const { value, interpretation } = homaIR({
 
 console.log(`HOMA-IR: ${value} (${interpretation})`);
 
-// Throws errors for invalid inputs
-try {
-  homaIR({
-    glucose: 0,
-    insulin: 0,
-    fasting: false,
-  });
-} catch (e) {
-  console.error(`Error: ${e.message}`);
+// Throws an error if not fasting
+generateHomaIrError({
+  glucose: 100,
+  insulin: 10,
+  fasting: false,
+});
+
+// Throws an error if glucose is less than or equal to 0
+generateHomaIrError({
+  glucose: 100,
+  insulin: 0,
+  fasting: true,
+});
+
+// Throws an error if insulin is less than or equal to 0
+generateHomaIrError({
+  glucose: 0,
+  insulin: 10,
+  fasting: true,
+});
+
+function generateHomaIrError({ glucose, insulin, fasting }) {
+  try {
+    homaIR({ glucose, insulin, fasting });
+  } catch (e) {
+    console.error(`Error: ${e.message}`);
+  }
 }
